b2894f7115373a9d297fa91afcab52f31cef40c7
katelee <cminlee9@gmail.com>
2018-10-27 17:04:54 +0800

    .

--- passport/facebook.js
+++ passport/facebook.js
@@ -16,20 +16,18 @@ passport.use(new FacebookStrategy({
         clientID: 1900216703426978,
         clientSecret: 'f2c2ee6069de323109cb9347fad01026',
         callbackURL: "https://kate-pocketdoctor.herokuapp.com/passport/facebook/callback",
-        profileFields: ['id', 'name', 'email'],
         passReqToCallback: true
     },
     function (req, accessToken, refreshToken, profile, done) {
 
-        var values = {facebook: profile.id , name: profile.user};
+        var values = {facebook: profile.id , name: profile.name};
             User.findOrCreate({where: {facebook: profile.id}, values})
                 .spread((user, created) => {
                         console.log(user.get({
                             plain: true
                         }));
-                        console.log( "this is second console.log" + created);
+                        console.log(created);
                         console.log(JSON.stringify(profile));
-                        console.log(JSON.stringify(accessToken));
                 })
 
         // User.findOne({
