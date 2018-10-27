dd73ae5950cac20a2e83a6aa825e0804fc7daa97
katelee <cminlee9@gmail.com>
2018-10-27 23:40:15 +0800

    hook on sequelize in init()

--- passport/init.js
+++ passport/init.js
@@ -1,6 +1,11 @@
 var passport = require('passport');
-var User = require('../models/user');
-
+// var User = require('../models/user');
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('heroku_5b563746e9de2a7','bc3248bee2e38f', "8823eb86", {
+    host: 'us-cdbr-iron-east-01.cleardb.net',
+    dialect: 'mysql',
+});
+const User = require('../models/user')(sequelize, Sequelize);
 
 module.exports = function() {
 
