fbbb59a79ac1f179a34c5bbdca08257503c27ebc
katelee <cminlee9@gmail.com>
2018-10-27 21:51:08 +0800

    configure routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 
 var passport = require('passport');
@@ -21,13 +22,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 //passport middleware
-app.use(session({
+app.use(express.session({
     secret: 'keyboard cat',
     resave: true,
     saveUninitialized: true
@@ -35,6 +37,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(app.router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
